331312b41a89355ed7a156cc0a440205cd2cd87c
harnain <harnain33@gmail.com>
2025-02-28 13:08:10 -0500

    adding all questions

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Question1 from './Question1';  
+import Question2 from './Question2';  
+import Question3 from './Question3';  
+import Question4 from './Question4';  
+import Question5 from './Question5';  
+import Question6 from './Question6';  
+import Question7 from './Question7';  
+import Question8 from './Question8';  
+import Question9 from './Question9';  
+import Question10 from './Question10'; 
+ 
 import QResults from './QResults';   
 import Dashboard from './dashboard'; 
 
