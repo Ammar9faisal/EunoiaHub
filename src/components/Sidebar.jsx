9573f666bdb2ae658d436a66dcaf6a2719841c50
JakPrz <jakub.przystupa02@gmail.com>
2025-03-07 21:45:41 -0500

    Improved sidebar and implemented it to user profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,51 +1,58 @@
 import React, { useState } from 'react';
-import { BarChart2, Smile, NotepadText , Settings, LogOut } from 'lucide-react';
+import { BarChart2, Smile, NotepadText, Settings, LogOut, FileText } from 'lucide-react'; 
 import './sidebar.css';
 import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
 
 export function Sidebar() {
-  const [activeButton, setActiveButton] = useState('dashboard');  // 'dashboard' is the default active button
-  const navigate = useNavigate(); // useNavigate is a hook that returns a navigate function to navigate to a different route
+  const [activeButton, setActiveButton] = useState('dashboard');
+  const navigate = useNavigate();
 
-  const handleButtonClick = (buttonName) => {  // sets the button as active when a button is clicekd
+  const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
   };
 
   return (
     <div className="sidebar">
-      <img className="sidebar-logo" src={logo} />
+      <img className="sidebar-logo" src={logo} alt="Logo" />
       <nav className="sidebar-nav">
         <button
           className={`sidebar-button ${activeButton === 'dashboard' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('dashboard')}  // sets the button as active when a button is clicekd
+          onClick={() => navigate("/dashboard")}
         >
           <BarChart2 className="sidebar-icon" color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'checkin' ? 'sidebar-button-active' : ''}`}
-          onClick={() => navigate("/survey")}  // sets the button as active when a button is clicekd
+          onClick={() => navigate("/survey")}
         >
-          <Smile className="sidebar-icon"  color="white"/>
+          <Smile className="sidebar-icon" color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'users' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('users')} // sets the button as active when a button is clicekd
+          onClick={() => handleButtonClick('users')}
         >
-          <NotepadText className="sidebar-icon"  color="white"/>
+          <NotepadText className="sidebar-icon" color="white"/>
         </button>
         <button
           className={`sidebar-button ${activeButton === 'settings' ? 'sidebar-button-active' : ''}`}
-          onClick={() => handleButtonClick('settings')}   // sets the button as active when a button is clicekd
+          onClick={() => handleButtonClick('settings')}
         >
-          <Settings className="sidebar-icon"  color="white"/>
+          <Settings className="sidebar-icon" color="white"/>
+        </button>
+        <button
+          className={`sidebar-button ${activeButton === 'blog' ? 'sidebar-button-active' : ''}`}
+          onClick={() => navigate("/blog")}
+        >
+          <FileText className="sidebar-icon" color="white"/> 
         </button>
       </nav>
       <button className="sidebar-button" onClick={() => navigate('/')}>
-        <LogOut className="sidebar-icon"  color="white"/>
+        <LogOut className="sidebar-icon" color="white"/>
       </button>
     </div>
   );
 }
 
 export default Sidebar;
+
