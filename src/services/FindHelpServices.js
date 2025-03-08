33913a971ecac21b680acaa9943ae80216cf85c4
Manjot <manukaur1999@gmail.com>
2025-03-08 02:00:41 -0500

    Made some changes to find help

--- src/services/FindHelpServices.js
+++ src/services/FindHelpServices.js
@@ -71,11 +71,8 @@ export const handleSearch = async (
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-<<<<<<< HEAD
           'X-Goog-Api-Key': 'KEY', // *************************PUT KEY (Consider using environment variables)
-=======
-          'X-Goog-Api-Key': 'AIzaSyDQ7eM3Y-o2I_aYEnvw8jEATtU8fyvyDXY', // PUT KEY (Consider using environment variables)
->>>>>>> c58a083 (Added find help to sidebar)
+        
           'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.primaryType,places.location',
         },
         body: JSON.stringify(requestBody),
@@ -126,11 +123,8 @@ export const handleSearch = async (
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-<<<<<<< HEAD
-              'X-Goog-Api-Key': 'KEy', // *****************PUT KEY (Consider using environment variables)
-=======
-              'X-Goog-Api-Key': 'AIzaSyDQ7eM3Y-o2I_aYEnvw8jEATtU8fyvyDXY', // PUT KEY (Consider using environment variables)
->>>>>>> c58a083 (Added find help to sidebar)
+              'X-Goog-Api-Key': 'KEY', // *****************PUT KEY (Consider using environment variables)
+              
               'X-Goog-FieldMask': 'routes.distanceMeters',
             },
             body: JSON.stringify(routesRequestBody),
