5892a25f342e10b23ad7fa981e8b2703cd2ffd04
Manjot <manukaur1999@gmail.com>
2025-03-08 01:28:39 -0500

    Separated UI and logic

--- src/services/FindHelpServices.js
+++ src/services/FindHelpServices.js
@@ -71,7 +71,7 @@ export const handleSearch = async (
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'X-Goog-Api-Key': 'AIzaSyDQ7eM3Y-o2I_aYEnvw8jEATtU8fyvyDXY', // PUT KEY (Consider using environment variables)
+          'X-Goog-Api-Key': 'KEY', // *************************PUT KEY (Consider using environment variables)
           'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.primaryType,places.location',
         },
         body: JSON.stringify(requestBody),
@@ -122,7 +122,7 @@ export const handleSearch = async (
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'X-Goog-Api-Key': 'AIzaSyDQ7eM3Y-o2I_aYEnvw8jEATtU8fyvyDXY', // PUT KEY (Consider using environment variables)
+              'X-Goog-Api-Key': 'KEy', // *****************PUT KEY (Consider using environment variables)
               'X-Goog-FieldMask': 'routes.distanceMeters',
             },
             body: JSON.stringify(routesRequestBody),
