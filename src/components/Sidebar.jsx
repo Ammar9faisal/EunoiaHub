ea72a383b6dab82ed2e9001f9fff31819e8fda65
harnain <harnain33@gmail.com>
2025-03-09 12:30:36 -0400

    adding to do list to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BarChart2, Smile, NotepadText , Settings, LogOut, Star } from 'lucide-react';
+import { BarChart2, Smile, NotepadText , Settings, LogOut, Star, LayoutList } from 'lucide-react';
 import './sidebar.css';
 import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
@@ -46,6 +46,13 @@ export function Sidebar() {
           onClick={() => handleButtonClick('UserTickets')}   // sets the button as active when a button is clicekd
         >
           <Star className="sidebar-icon"  color="white"/>
+        </button>
+
+        <button
+          className={`sidebar-button ${activeButton === 'ToDoList' ? 'sidebar-button-active' : ''}`}
+          onClick={() => handleButtonClick('ToDoList')}   // sets the button as active when a button is clicekd
+        >
+          <LayoutList className="sidebar-icon"  color="white"/>
         </button>
 
       </nav>
